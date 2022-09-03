d1595a8e341dccea5df2324307a1d39db5622b72
Duberly Guarnizo <duberlygfr@gmail.com>
2022-09-03 15:15:19 -0500

    Updated component creation process. Graph types are now created from lists. Changed css styling for site. Every graph type selector now is bound to a number to call a function later (WIP)

--- src/app/pdasite/main-content/main-content.component.ts
+++ src/app/pdasite/main-content/main-content.component.ts
@@ -50,4 +50,5 @@ export class MainContentComponent implements OnInit {
     }
   }
 
+
 }
--- src/app/pdasite/main-graph-selector/main-graph-selector.component.ts
+++ src/app/pdasite/main-graph-selector/main-graph-selector.component.ts
@@ -7,10 +7,22 @@ import {Component, OnInit} from '@angular/core';
 })
 export class MainGraphSelectorComponent implements OnInit {
 
+  graphs: any[];
+
   constructor() {
+    this.graphs = [
+      {id: 0, title: 'SWOT', description: 'Shows the strength and some things more'},
+      {id: 1, title: 'Lean Canvas', description: 'A pretty little chart'},
+      {id: 2, title: 'Fishi Fish', description: 'When you have time to waste in graphing'},
+      {id: 3, title: 'Laziness Graph', description: 'When you have time to waste in graphing'},
+      {id: 4, title: 'Moar Mngmt Craziness', description: 'Management wants moar graphs!'}
+    ];
   }
 
   ngOnInit(): void {
   }
 
+  graphSelected(id: number): void {
+    console.log(id);
+  }
 }
